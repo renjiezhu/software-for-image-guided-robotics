5dce4e9b4be139a03cd9ac2de9dcdcd4b7a7c96d
Renjie Zhu <renjie.j.zhu@gmail.com>
2019-07-03 10:58:57 -0700

    node running properly, dependency solved

--- src/world_to_base_client.cpp
+++ src/world_to_base_client.cpp
@@ -6,7 +6,7 @@
 #include "geometry_msgs/Transform.h"
 #include "geometry_msgs/Twist.h"
 
-// #include "software_interface/worldToBase.h"
+#include "software_interface/worldToBase.h"
 
 bool worldToBase(geometry_msgs::QuaternionPtr quaternion) {
     return true;
@@ -14,13 +14,13 @@ bool worldToBase(geometry_msgs::QuaternionPtr quaternion) {
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "worldToBase");
+    ros::init(argc, argv, "world_to_base");
 
     // handle any argument errors
 
     ros::NodeHandle n;
 
-    // ros::ServiceClient client = n.serviceClient<software_interface::worldToBase>("worldToBase");
-
+    ros::ServiceClient client = n.serviceClient<software_interface::worldToBase>("world_to_base");
+    ROS_INFO("client created.--- test");
     return 0;
 }
