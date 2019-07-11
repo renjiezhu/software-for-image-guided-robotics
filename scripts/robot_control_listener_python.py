75b838879f67293abcac86b514b46dd65e1fa219
Renjie Zhu <renjie.j.zhu@gmail.com>
2019-07-10 17:36:16 -0700

    managed frame  switch

--- scripts/robot_control_listener_python.py
+++ scripts/robot_control_listener_python.py
@@ -8,10 +8,16 @@ def test_callback(data):
     rospy.loginfo("test: x = %d" % data.linear.x)
 
 def vrep_callback(data):
-    pos = np.array([data.linear.x, data.linear.y, data.linear.z])
-    ori = np.array([data.angular.x, data.angular.y, data.angular.z])
-    rospy.loginfo(pos)
-    rospy.loginfo(ori)
+    pos_screen = np.array([data.linear.x, data.linear.y, data.linear.z])
+    ori_screen = np.array([data.angular.x, data.angular.y, data.angular.z])
+    rospy.logdebug(pos_screen)
+    rospy.logdebug(ori_screen)
+
+    pos_robot = np.array([pos_screen[1]*-1, pos_screen[0], pos_screen[2]])
+    ori_robot = np.array([ori_screen[1]*-1, ori_screen[0], ori_screen[2]])
+
+    # call vrep set
+
 
 
 def listener():
---
+++ scripts/robot_motor_controller.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+from geometry_msgs.msg import Twist
+
+# constantly get from vrep and set angles to fpga
